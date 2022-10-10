e92bc3a8ad91387e63d98458912c1b9ff9625b8b
zjh1850283352 <1850283352@qq.com>
2022-10-10 12:36:54 +0800

    20221010

--- based-face-recognition/10人脸识别.py
+++ based-face-recognition/10人脸识别.py
@@ -51,6 +51,8 @@ def warning():
     print(statusStr[the_page])
 
 
+
+
 #准备识别的图片
 def face_detect_demo(img):
     gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)#转换为灰度
@@ -59,19 +61,10 @@ def face_detect_demo(img):
     face=face_detector.detectMultiScale(gray)
     for x,y,w,h in face:
         cv2.rectangle(img,(x,y),(x+w,y+h),color=(0,0,255),thickness=2)
-        #cv2.circle(img,center=(x+w//2,y+h//2),radius=w//2,color=(0,255,0),thickness=1)
         # 人脸识别
         ids, confidence = recogizer.predict(gray[y:y + h, x:x + w])
         #print('标签id:',ids,'置信评分：', confidence)
-        '''if confidence > 80:
-            global warningtime
-            warningtime += 1
-            if warningtime > 100:
-               warning()
-               warningtime = 0
-            cv2.putText(img, 'unkonw', (x + 10, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 0), 1)
-        else:
-            cv2.putText(img,str(names[ids-1]), (x + 10, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 0), 1)'''
+        cv2.putText(img,str(names[ids-1]), (x + 10, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 0), 1)
     cv2.imshow('result',img)
     #print('bug:',ids)
 
@@ -82,7 +75,6 @@ def name():
     for imagePath in imagePaths:
        name = str(os.path.split(imagePath)[1].split('.',2)[1])
        names.append(name)
-
 
 cap=cv2.VideoCapture(0)
 name()
@@ -98,14 +90,5 @@ while True:
     if cv2.waitKey(1) == ord('q'):
         break
 
-'''name()
-while True:
-    flag,frame=cap.read()
-    if not flag:
-        break
-    face_detect_demo(frame)
-    if ord(' ') == cv2.waitKey(10):
-        break'''
 cv2.destroyAllWindows()
 cap.release()
-#print(names)
